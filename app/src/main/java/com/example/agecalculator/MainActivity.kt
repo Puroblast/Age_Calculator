35edbf29c2d443b3a30d8767b1927bc5b7afd9cc
Puroblast <Puroblast97@mail.ru>
2022-10-09 02:44:11 +0300

    did some changes

--- app/src/main/java/com/example/agecalculator/MainActivity.kt
+++ app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val cal = Calendar.getInstance()
-        val day = cal.get(Calendar.YEAR)
+        val day = cal.get(Calendar.DAY_OF_MONTH)
         val month = cal.get(Calendar.MONTH)
         val year = cal.get(Calendar.YEAR)
         val dsl =
@@ -34,9 +34,9 @@ class MainActivity : AppCompatActivity() {
             DatePickerDialog(
                 this,
                 dsl,
-                cal.get(Calendar.YEAR),
-                cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH)
+                year,
+                month,
+                day
             ).show()
         }
     }
