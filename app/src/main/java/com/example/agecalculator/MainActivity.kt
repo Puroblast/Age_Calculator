b867ae8d40e5b013f803e09716b7cbef3e18677b
Puroblast <Puroblast97@mail.ru>
2022-10-09 23:50:12 +0300

    Сделал немного апгрейдов кода, вынес некоторые действия в отдельные функции, изменил названия переменных. Перехожу к след проекту

--- app/src/main/java/com/example/agecalculator/MainActivity.kt
+++ app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -7,38 +7,49 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.agecalculator.databinding.ActivityMainBinding
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.Calendar.*
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by viewBinding()
+    private val myFormat = SimpleDateFormat("dd/MM/yyyy", Locale.US)
+    private val calendar = Calendar.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val cal = Calendar.getInstance()
-        val day = cal.get(Calendar.DAY_OF_MONTH)
-        val month = cal.get(Calendar.MONTH)
-        val year = cal.get(Calendar.YEAR)
-        val dsl =
-            DatePickerDialog.OnDateSetListener { view, currentYear, currentMonth, currentDay ->
-                cal.set(Calendar.YEAR, currentYear)
-                cal.set(Calendar.MONTH, currentMonth)
-                cal.set(Calendar.DAY_OF_MONTH, currentDay)
+        initViews()
+    }
 
-                binding.selectedDateView.text = "Selected Date"
-                binding.dateView.text = SimpleDateFormat("dd/MM/yyyy", Locale.US).format(cal.time)
+    private fun initViews(){
+        val day = calendar.get(DAY_OF_MONTH)
+        val month = calendar.get(MONTH)
+        val year = calendar.get(YEAR)
+        val datesetlistener =
+            DatePickerDialog.OnDateSetListener { _, selectedYear, selectedMonth, selectedDay ->
+                calendar.set(YEAR, selectedYear)
+                calendar.set(MONTH, selectedMonth)
+                calendar.set(DAY_OF_MONTH, selectedDay)
+
+                binding.selectedDateView.text = getString(R.string.selectedDateView)
+                binding.dateView.text = myFormat.format(calendar.time)
                 binding.totalMinutesView.text =
-                    ((year * 525600 + month * 43800 + day * 1440) - (currentYear * 525600 + currentMonth * 43800 + currentDay * 1440)).toString()
-                binding.tillDateView.text = "In minutes till date"
+                    "${toMinutes(year,month,day) - toMinutes(selectedYear,selectedMonth,selectedDay)}"
+                binding.tillDateView.text = getString(R.string.tillDateView)
 
             }
         binding.selectDateBtn.setOnClickListener {
             DatePickerDialog(
                 this,
-                dsl,
+                datesetlistener,
                 year,
                 month,
                 day
             ).show()
         }
     }
+
+    private fun toMinutes(year:Int,month:Int,day:Int): Int {
+        return (year * 525600 + month * 43800 + day * 1440)
+    }
+
 
 }
